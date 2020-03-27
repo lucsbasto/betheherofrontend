51e5f68377f203533e4a4c6185105ff3bda0d557
Lucas Bastos <lucasbastos@gmail.com>
2020-03-27 16:38:04 -0300

    criando e estilizando paginas de login e register

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./global.css";
+import Login from "./pages/Login";
+import Routes from "./routes";
+function App() {
+  return <Routes />;
+}
+
+export default App;
